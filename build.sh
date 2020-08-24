4ebddc4e1cca03a42df0b4b9e2b7722954033639
Brian Burke <brian.burke@anz.com>
2020-08-24 10:07:08 +1000

    Testing githu actions

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+OUTPUT="properties"
+readonly OUTPUT
+
+# Update docker image and populate with latest jsonnet files
+docker build -t jsonnet/build .
+
+find . -type f -iname "_compose.jsonnet" -print0 | while IFS= read -r -d $'\0' file; do
+  tmp="${file/jsonnet/${OUTPUT}}"
+  outputPath="${tmp/_compose.jsonnet/}"
+  mkdir -pv $outputPath
+  docker run -v $(pwd)/${OUTPUT}:/usr/src/${OUTPUT} jsonnet/build:latest $file -m $outputPath
+done
+
+node styleDictionary
